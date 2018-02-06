9b9e17212c3d32ddedb5cc7aa38e7106873e57f5
yoshiki <interestingyoshiki@gmail.com>
2018-02-07 01:34:49 +0900

    ITP_1_2_A 再度修正

--- introduction/ITP1_2_A.rs
+++ introduction/ITP1_2_A.rs
@@ -3,15 +3,15 @@ fn main() {
 	let mut s = String::new();
 	stdin.read_line(&mut s).unwrap();
 	
-	let input = s.trim().split(" ").collect::<Vec<i32>>();
+	let input = s.trim().split(" ").map(|c| c.parse::<i32>().unwrap()).collect::<Vec<i32>>();
 	let a = input[0];
 	let b = input[1];
 	
 	if a < b {
-		println!("{} < {}", a, b);
+		println!("a < b");
 	}else if a > b {
-		println!("{} > {}", a, b);
+		println!("a > b");
 	}else{
-		println!("{} == {}", a, b);
+		println!("a == b");
 	}
 }
